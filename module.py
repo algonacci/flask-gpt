131c3787f4cd13c18ef4e4d68a2281dbc1b885e9
Eric Julianto <algonacci1@gmail.com>
2023-03-14 10:58:30 +0700

    Finishing

---
+++ module.py
@@ -0,0 +1,19 @@
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+openai.api_key = config["OPENAI_API_KEY"]
+
+
+def generate(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        timeout=15
+    )
+    return response.choices[0].text.strip()
